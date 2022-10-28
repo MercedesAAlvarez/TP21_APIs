69f4e504b9cebd005b52c72de229bad35dce382b
MercedesAAlvarez <mechychevy@gmail.com>
2022-10-28 17:22:05 -0300

    realizando tp apis

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ const app = express();
 const indexRouter = require('./routes/index');
 const moviesRoutes = require('./routes/moviesRoutes');
 const genresRoutes = require('./routes/genresRoutes');
+const genresRoutesApi = require('./routes/apiRouter/genresApiRoutes');
 
 //Aquí pueden colocar las rutas de las APIs
 
@@ -27,6 +28,7 @@ app.use(methodOverride('_method'));
 app.use('/', indexRouter);
 app.use(moviesRoutes);
 app.use(genresRoutes);
+app.use('/api',genresRoutesApi);
 
 
 //Activando el servidor desde express
